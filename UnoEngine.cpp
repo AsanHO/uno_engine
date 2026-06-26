13a455412de0fa7336f2efb893060426a76610a5
AsanHo <gorma00@naver.com>
2026-06-26 15:11:07 +0900

    기존 오브젝트렌더링 코드 제거

--- UnoEngine.cpp
+++ UnoEngine.cpp
@@ -131,48 +131,6 @@ bool UnoEngine::Initialize() {
                              L"./Assets/Textures/Cubemaps/HDRI/indoorSpecularHDR.dds",
                              L"./Assets/Textures/Cubemaps/HDRI/indoorDiffuseHDR.dds",
                              L"./Assets/Textures/Cubemaps/HDRI/indoorBrdf.dds"); 
-    // Geometry 정의
-    auto [vertices, indices] = MakeBox();
-
-    // 버텍스 버퍼 만들기
-    D3D11Utils::CreateVertexBuffer(m_device, vertices, m_vertexBuffer);
-
-    // 인덱스 버퍼 만들기
-    m_indexCount = UINT(indices.size());
-
-    D3D11Utils::CreateIndexBuffer(m_device, indices, m_indexBuffer);
-
-    // ConstantBuffer 만들기
-    m_constantBufferData.model = Matrix();
-    m_constantBufferData.view = Matrix();
-    m_constantBufferData.projection = Matrix();
-
-    EngineBase::CreateConstantBuffer(m_constantBufferData, m_constantBuffer);
-
-    // 쉐이더 만들기
-
-    // Input-layout objects describe how vertex buffer data is streamed into the
-    // IA(Input-Assembler) pipeline stage.
-    // https://learn.microsoft.com/en-us/windows/win32/api/d3d11/nf-d3d11-id3d11devicecontext-iasetinputlayout
-
-    // Input-Assembler Stage
-    // The purpose of the input-assembler stage is to read primitive data
-    // (points, lines and/or triangles) from user-filled buffers and assemble
-    // the data into primitives that will be used by the other pipeline stages.
-    // https://learn.microsoft.com/en-us/windows/win32/direct3d11/d3d10-graphics-programming-guide-input-assembler-stage
-
-    vector<D3D11_INPUT_ELEMENT_DESC> inputElements = {
-        {"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
-        {"NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0},
-        {"TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 24, D3D11_INPUT_PER_VERTEX_DATA, 0},
-        {"TANGENT", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 32, D3D11_INPUT_PER_VERTEX_DATA, 0},
-        {"COLOR", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 44, D3D11_INPUT_PER_VERTEX_DATA, 0},
-    };
-
-    D3D11Utils::CreateVertexShaderAndInputLayout(m_device, L"ColorVertexShader.hlsl", inputElements,
-                                                 m_colorVertexShader, m_colorInputLayout);
-
-    D3D11Utils::CreatePixelShader(m_device, L"ColorPixelShader.hlsl", m_colorPixelShader);
 
     return true;
 }
@@ -201,30 +159,16 @@ void UnoEngine::Update(float dt) {
 
     // 큐브 매핑 Constant Buffer 업데이트
     m_cubeMapping.UpdateViewProjConstBuffer(m_device, m_context, viewRow, projRow);
-    // 모델의 변환
-    m_constantBufferData.model = Matrix::CreateScale(0.5f) * Matrix::CreateRotationY(rot) *
-                                 Matrix::CreateTranslation(Vector3(0.0f, -0.3f, 1.0f));
-    m_constantBufferData.model = m_constantBufferData.model.Transpose();
 
     using namespace DirectX;
 
     // 시점 변환 todo:: 카메라 클래스 만들기
     m_constantBufferData.view = m_camera.GetViewRow().Transpose();
 
-    // 프로젝션
-    /*const float aspect = AppBase::GetAspectRatio();
-    if (m_usePerspectiveProjection) {
-        const float fovAngleY = 70.0f * XM_PI / 180.0f;
-        m_constantBufferData.projection =
-            XMMatrixPerspectiveFovLH(fovAngleY, aspect, 0.01f, 100.0f);
-    } else {
-        m_constantBufferData.projection =
-            XMMatrixOrthographicOffCenterLH(-aspect, aspect, -1.0f, 1.0f, 0.1f, 10.0f);
-    }*/
+    
     m_constantBufferData.projection = m_camera.GetProjRow().Transpose();
 
-    // Constant를 CPU에서 GPU로 복사
-    D3D11Utils::UpdateBuffer(m_device, m_context, m_constantBufferData, m_constantBuffer);
+    
 }
 
 void UnoEngine::Render() {
@@ -250,28 +194,7 @@ void UnoEngine::Render() {
 
     m_context->OMSetDepthStencilState(m_depthStencilState.Get(), 0);
     m_cubeMapping.Render(m_context, false);
-    // 어떤 쉐이더를 사용할지 설정
-    m_context->VSSetShader(m_colorVertexShader.Get(), 0, 0);
-
-    /* 경우에 따라서는 포인터의 배열을 넣어줄 수도 있습니다.
-    ID3D11Buffer *pptr[1] = {
-        m_constantBuffer.Get(),
-    };
-    m_context->VSSetConstantBuffers(0, 1, pptr); */
-
-    m_context->VSSetConstantBuffers(0, 1, m_constantBuffer.GetAddressOf());
-    m_context->PSSetShader(m_colorPixelShader.Get(), 0, 0);
-
-    m_context->RSSetState(m_rasterizerSate.Get());
-
-    // 버텍스/인덱스 버퍼 설정
-    UINT stride = sizeof(Vertex);
-    UINT offset = 0;
-    m_context->IASetInputLayout(m_colorInputLayout.Get());
-    m_context->IASetVertexBuffers(0, 1, m_vertexBuffer.GetAddressOf(), &stride, &offset);
-    m_context->IASetIndexBuffer(m_indexBuffer.Get(), DXGI_FORMAT_R32_UINT, 0);
-    m_context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-    m_context->DrawIndexed(m_indexCount, 0, 0);
+   
 }
 
 void UnoEngine::UpdateGUI() {
